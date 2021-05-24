d471917e101116d9e0348b6dd79d3383b5229820
sebringj <mail@jasonsebring.com>
2021-05-23 18:35:55 -0700

    initial

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,160 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect, useRef, CSSProperties} from 'react';
 import './App.css';
+import '@tensorflow/tfjs'
+import '@tensorflow/tfjs-backend-cpu'
+import '@tensorflow/tfjs-backend-webgl'
+import * as mobilenet from '@tensorflow-models/mobilenet'
+import * as cocoSsd from '@tensorflow-models/coco-ssd'
+import * as toxicity from '@tensorflow-models/toxicity'
+import * as nsfwjs from 'nsfwjs'
 
 function App() {
+  const [imgSrc, setImgSrc] = useState('')
+  const [text, setText] = useState('')
+  const [classifyImage, setClassifyImage] = useState<{
+    className: string;
+    probability: number;
+  }[]>()
+  const [nudifyImage, setNudityImage] = useState<nsfwjs.predictionType[]>()
+  const [objectDetection, setObjectDetection] = useState<cocoSsd.DetectedObject[]>()
+  const classifyModel = useRef<mobilenet.MobileNet>()
+  const objectModel = useRef<cocoSsd.ObjectDetection>()
+  const nudityModel = useRef<nsfwjs.NSFWJS>()
+  const [classifyText, setClassifyText] = useState<{
+    label: string;
+    results: {
+        probabilities: Float32Array;
+        match: boolean;
+    }[];
+}[]>()
+  const toxicityModel = useRef<toxicity.ToxicityClassifier>()
+  useEffect(() => {
+    async function setModel() {
+      const [cm, om, tm, nm] = await Promise.all([
+        mobilenet.load(),
+        cocoSsd.load(),
+        toxicity.load(0.9, [
+          'identity_attack',
+          'insult',
+          'obscene',
+          'severe_toxicity',
+          'sexual_explicit',
+          'threat',
+          'toxicity'
+        ]),
+        nsfwjs.load('/models/nsfw/', { size: 299 })
+      ])
+      classifyModel.current = cm
+      objectModel.current = om
+      toxicityModel.current = tm
+      nudityModel.current = nm
+      console.log('nm', nm)
+      console.log('loaded models')
+    }
+    setModel()
+  }, [])
+  const checkImage = async () => {
+    const img = new Image()
+    img.crossOrigin = 'anonymous'
+    function catchIt(err: Error) {
+      console.log(err)
+      return undefined
+    }
+    img.addEventListener('load', async () => {
+      const [o, c, n] = await Promise.all([
+        objectModel.current?.detect(img).catch(catchIt),
+        classifyModel.current?.classify(img).catch(catchIt),
+        nudityModel.current?.classify(img).catch(catchIt)
+      ])
+      setObjectDetection(o)
+      setClassifyImage(c)
+      console.log('n', n)
+      setNudityImage(n)
+    })
+    img.addEventListener('error', (err) => {
+      console.log(err)
+    })
+    img.src = imgSrc
+  }
+  const checkText = async () => {
+    setClassifyText(await toxicityModel.current?.classify(text))
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <label>
+        <span>Enter Image Source</span>
+        <input type="text" value={imgSrc} onChange={e => setImgSrc(e.currentTarget.value)}  />
+      </label>
+      <button onClick={checkImage}>Check Image</button>
+      {classifyImage && <div>
+        <p>Classify Image</p>
+        {classifyImage?.map((p, i) => {
+          return (
+            <div key={`${i}`}>
+              <div>{p.className} {p.probability}</div>
+            </div>
+          )
+        })}
+      </div>}
+      {objectDetection && <div>
+        <p>Image Detect Objects</p>
+        {objectDetection?.map((p, i) => {
+          return (
+            <div key={`${i}`}>
+              <div>{p.class} {p.score}</div>
+            </div>
+          )
+        })}
+      </div>}
+      {nudifyImage && <div>
+        <p>Image Nudity Detection</p>
+        {nudifyImage?.map((p, i) => {
+          return (
+            <div key={`${i}`}>
+              <div>{p.className} {p.probability}</div>
+            </div>
+          )
+        })}
+      </div>}
+      <div style={styles.textContainer}>
+        <label>
+          <span>Enter text</span>
+          <textarea
+            value={text}
+            onChange={e => setText(e.currentTarget.value)}
+          />
+        </label>
+      </div>
+      <button onClick={checkText}>Check Text</button>
+      {classifyText && <table style={styles.classifyTextContainer}>
+        <thead>Classify Text</thead>
+        {classifyText?.map((p, i) => {
+          return (
+              <tr key={`${i}`}>{p.results.map((r, j) => {
+                return (
+                  <td key={`${j}`} style={r.match ? { color: 'red' } : {}}>{p.label}</td>
+                )
+              })}
+            </tr>
+          )
+        })}
+      </table>}
     </div>
   );
 }
 
 export default App;
+
+const styles: { [key: string]: CSSProperties } = {
+  textContainer: {
+    marginTop: 40,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  classifyTextContainer: {
+    display: 'table',
+    justifySelf: 'center',
+    alignItems: 'center'
+  }
+}
